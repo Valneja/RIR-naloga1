c06a21c6508bac16794e925208ea5f62b1dfb9d4
Valneja Stojcic <valneja.stojcic.extern@sixt.com>
2019-10-29 08:31:52 +0100

    added comments to code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 
-
+// vrne vsa števila, ki so bila do sedaj vnešena
 function AllNumbers(props) {
   const numbers = props.numbers.join(", ");
   return(
@@ -10,6 +10,7 @@ function AllNumbers(props) {
   );
 }
 
+// vrne gumb za dodajanje števil
 function AddNumberButton(props) {
   return (
     <Button variant="info" type="submit">
@@ -18,6 +19,7 @@ function AddNumberButton(props) {
   );
 }
 
+// vrne gumb za izračun zahtevanih vrednosti
 function ComputeButton(props) {
   return (
     <Button variant="info" onClick={props.onClick}>
@@ -26,6 +28,7 @@ function ComputeButton(props) {
   );
 }
 
+// vrne gumb za brisanje do sedaj vnešenih števil
 function ClearButton(props) {
   return (
     <Button variant="info" onClick={props.onClick}>
@@ -34,6 +37,7 @@ function ClearButton(props) {
   );
 }
 
+// če je vrednost številska vrne true, drugače false
 function validateValue(value) {
   if (!value || isNaN(value)) {
     return false;
@@ -41,41 +45,58 @@ function validateValue(value) {
   return true;
 }
 
+// vrne aritmetično sredino
 function ArithmeticMean(values) {
     const count = values.length;
     let sum = 0;
-    for(var i in values) { sum += values[i]; console.log(i);}
+    for(var i in values) {
+      sum += values[i]; console.log(i);
+    }
     const mean = sum/count;
     return mean.toFixed(10);
 }
 
+// vrne števila razvrščena od največjega do najmanjšega
 function BubbleSort(values) {
+  // naredi kopijo števil v values
     let sortValues = values.slice();
     let n = sortValues.length;
+    // izvajaj dokler niso vsa števila na urejena (dokler ni tudi število z najmanjšim indeksom na pravem mestu)
     while(n > 1) {
       let newn = 0;
+      // preglej vsa števila od leve proti desni
       for (let i = 1; i < n; i++) {
+        // če je prejšnje število manjše od trenutnega ju zamenjaj (porini manjše število na desno)
         if (sortValues[i-1] < sortValues[i]) {
           let firstValue = sortValues[i-1];
           sortValues[i-1] = sortValues[i];
           sortValues[i] = firstValue;
+          // shrani index števila, ki je bilo na zadnje premaknjeno (vsa števila desno od tega so že urejena po velikosti)
           newn = i;
         }
       }
+      // samo števila do indeksa newn še niso na svojem mestu (levo od tega indeksa)
       n = newn;
     }
     return sortValues;
 }
 
+// vrne standardni odklon
 function StandardDeviation(values, arithmeticMean) {
+    // število vnesenih števil
     const n = values.length;
+    // standardnega odklona ne moremo izračunati, če imamo samo eno vneseno število
     if (n === 1) {
       return false;
     }
     let compute = 0;
+    // pojdi čez vsa števila v tabeli values
     for (let i in values) {
+      // v spremenljivko compute prištej kvadrat razlike trenutne vrednosti in aritmetične sredine
       compute += Math.pow((values[i]-arithmeticMean), 2);
     }
+    // vrni koren vrednosti compute deljene z vrednostjo n - 1
+    // vse skupaj zaokroži na 10 decimalk
     return Math.sqrt(compute/(n-1)).toFixed(10);
 }
 
@@ -92,11 +113,13 @@ class MainPanel extends React.Component {
     }
   }
 
+// shrani novo vnešeno število
   addNumber() {
     const values = this.state.values.slice();
     const currentValue = this.state.inputValue;
     const validationOk = validateValue(currentValue);
     if (!validationOk) {
+      // če vnesen podatek ni število prikaže obvestilo
       this.setState({
         errors:"Vnesen podatek ni število. Prosimo vnesite število.",
       });
@@ -109,9 +132,10 @@ class MainPanel extends React.Component {
     });
   }
 
-
+// izračuna željene vrednosti
   compute() {
     const values = this.state.values;
+    // za izračun morata biti vneseni vsaj dve števili
     if (values.length < 2) {
       this.setState({
         errors:"Vnesite vsaj dva števila.",
@@ -135,6 +159,7 @@ class MainPanel extends React.Component {
       });
   }
 
+// počisti vsa vnešena števila in izračunane vrednosti
   clear() {
     this.setState({
       values: [],
